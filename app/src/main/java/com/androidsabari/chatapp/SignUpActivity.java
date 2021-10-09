04bbb9148ff099920dccc6a2e16b6e7b4c01fceb
Sabari <r.sabarinathan22@gmail.com>
2021-10-10 00:03:38 +0530

    Register photo

--- app/src/main/java/com/androidsabari/chatapp/SignUpActivity.java
+++ app/src/main/java/com/androidsabari/chatapp/SignUpActivity.java
@@ -1,8 +1,11 @@
 package com.androidsabari.chatapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -26,5 +29,36 @@ public class SignUpActivity extends AppCompatActivity {
         editTextPasswordSignup = findViewById(R.id.editTextPasswordSignup);
         editTextUsernameSignup = findViewById(R.id.editTextUserNameSignup);
         buttonRegister = findViewById(R.id.buttonRegister);
+
+        imageViewCircle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
+    }
+
+    public void imageChooser(){
+        Intent intent = new Intent();
+        intent.setType("images/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK && data != null)
     }
 }
