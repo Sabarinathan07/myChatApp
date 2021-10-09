a29dc194a5bfa2628ed0ae8088539786418617ae
Sabari <r.sabarinathan22@gmail.com>
2021-10-09 23:39:45 +0530

    Intent added

--- app/src/main/java/com/androidsabari/chatapp/MyLoginActivity.java
+++ app/src/main/java/com/androidsabari/chatapp/MyLoginActivity.java
@@ -1,12 +1,17 @@
 package com.androidsabari.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MyLoginActivity extends AppCompatActivity {
 
@@ -14,6 +19,7 @@ public class MyLoginActivity extends AppCompatActivity {
     private Button buttonSignin,buttonSignup;
     private TextView textViewForget;
 
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,38 @@ public class MyLoginActivity extends AppCompatActivity {
         buttonSignup = findViewById(R.id.buttonSignup);
         textViewForget = findViewById(R.id.textViewForget);
 
+        auth = FirebaseAuth.getInstance();
 
+        buttonSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyLoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        textViewForget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyLoginActivity.this,ForgetActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    public void signin(String email, String password){
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListner<AuthResult>()){
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task){
+        }
     }
 }
