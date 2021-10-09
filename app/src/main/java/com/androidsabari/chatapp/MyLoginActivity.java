349130bc60f95ec59f1972af6c0b9620f0a7e8cd
Sabari <r.sabarinathan22@gmail.com>
2021-10-09 23:54:31 +0530

    Authentication

--- app/src/main/java/com/androidsabari/chatapp/MyLoginActivity.java
+++ app/src/main/java/com/androidsabari/chatapp/MyLoginActivity.java
@@ -8,7 +8,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -37,6 +40,20 @@ public class MyLoginActivity extends AppCompatActivity {
         buttonSignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (!email.equals("") && !password.equals(""))
+                {
+                    signin(email,password);
+                }
+                else
+                {
+                    Toast.makeText(MyLoginActivity.this, "Please enter an email and password.", Toast.LENGTH_SHORT).show();
+                }
+
+
+
 
             }
         });
@@ -60,10 +77,25 @@ public class MyLoginActivity extends AppCompatActivity {
         });
     }
 
-    public void signin(String email, String password){
-        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListner<AuthResult>()){
+    public void signin(String email,String password)
+    {
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this,new OnCompleteListener<AuthResult>() {
             @Override
-            public void onComplete(@NonNull Task<AuthResult> task){
-        }
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful())
+                {
+                    Intent intent = new Intent(MyLoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(MyLoginActivity.this, "Sign in is successful.", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MyLoginActivity.this, "Sign in is not successful.", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
     }
 }
