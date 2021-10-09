3acbbeb328ad63fcb557e4a03f199161e84e848f
Sabari <r.sabarinathan22@gmail.com>
2021-10-09 20:48:38 +0530

    register ui added

--- app/src/main/java/com/androidsabari/chatapp/SignUpActivity.java
+++ app/src/main/java/com/androidsabari/chatapp/SignUpActivity.java
@@ -3,12 +3,28 @@ package com.androidsabari.chatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private CircleImageView imageViewCircle;
+    private TextInputEditText editTextEmailSignup,editTextPasswordSignup,editTextUsernameSignup;
+    private Button buttonRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        imageViewCircle = findViewById(R.id.imageViewCircle);
+        editTextEmailSignup = findViewById(R.id.editTextEmailSignup);
+        editTextPasswordSignup = findViewById(R.id.editTextPasswordSignup);
+        editTextUsernameSignup = findViewById(R.id.editTextUserNameSignup);
+        buttonRegister = findViewById(R.id.buttonRegister);
     }
 }
