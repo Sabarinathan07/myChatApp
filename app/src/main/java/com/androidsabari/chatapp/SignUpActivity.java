8b5f062da6fa7efe4454df715eed18a9bb87ea2d
Sabari <r.sabarinathan22@gmail.com>
2021-10-10 11:50:10 +0530

    Image can be choosen

--- app/src/main/java/com/androidsabari/chatapp/SignUpActivity.java
+++ app/src/main/java/com/androidsabari/chatapp/SignUpActivity.java
@@ -4,11 +4,13 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -17,6 +19,7 @@ public class SignUpActivity extends AppCompatActivity {
     private CircleImageView imageViewCircle;
     private TextInputEditText editTextEmailSignup,editTextPasswordSignup,editTextUsernameSignup;
     private Button buttonRegister;
+    Boolean imageControl = false;
 
 
     @Override
@@ -33,14 +36,20 @@ public class SignUpActivity extends AppCompatActivity {
         imageViewCircle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                imageChooser();
             }
         });
 
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email = editTextEmailSignup.getText().toString().trim();
+                String password = editTextPasswordSignup.getText().toString().trim();
+                String username = editTextEmailSignup.getText().toString().trim();
 
+                if(!email.equals("") && !password.equals("") && !username.equals("")){
+                    signup(email,password,username);
+                }
             }
         });
 
@@ -56,9 +65,20 @@ public class SignUpActivity extends AppCompatActivity {
         startActivityForResult(intent,1);
     }
 
+    public void signup(String email,String password,String username){
+
+    }
+
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable  Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 1 && resultCode == RESULT_OK && data != null)
+        if(requestCode == 1 && resultCode == RESULT_OK && data != null){
+            Uri imageUri = data.getData();
+            Picasso.get().load(imageUri).into(imageViewCircle);
+            imageControl= true;
+        }else{
+            imageControl = false;
+        }
     }
 }
