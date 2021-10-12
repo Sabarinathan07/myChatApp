5c790776a73a92d5711067b9f6b88d033beac17f
Sabarinathan07 <64265337+Sabarinathan07@users.noreply.github.com>
2021-10-12 11:28:35 +0530

    'design-for-messaging'

--- app/src/main/java/com/androidsabari/chatapp/MyChatActivity.java
+++ app/src/main/java/com/androidsabari/chatapp/MyChatActivity.java
@@ -1,14 +1,33 @@
 package com.androidsabari.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MyChatActivity extends AppCompatActivity {
+
+    private ImageView imageViewBack;
+    private TextView textViewChat;
+    private EditText editTextMessage;
+    private FloatingActionButton fab;
+    private RecyclerView rvChat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_chat);
+
+        imageViewBack = findViewById(R.id.imageViewBack);
+        textViewChat = findViewById(R.id.textViewChat);
+        editTextMessage = findViewById(R.id.editTextMessage);
+        fab = findViewById(R.id.fab);
+        rvChat = findViewById(R.id.rvChat);
+
     }
 }
